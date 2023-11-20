c8780b7da8d027690ca355566eb2cdc4bcc43438
wkj <1729298243@qq.com>
2023-11-20 19:27:27 +0800

    完成短信验证码登录功能

--- src/views/login.vue
+++ src/views/login.vue
@@ -25,7 +25,7 @@
           </a-input>
         </a-form-item>
         <a-form-item>
-          <a-button type="primary" block html-type="submit">登录</a-button>
+          <a-button type="primary" block html-type="submit" @click="login">登录</a-button>
         </a-form-item>
       </a-form>
     </a-col>
@@ -35,6 +35,7 @@
 <script setup>
 import { reactive } from "vue";
 import axios from "axios";
+import { notification } from "ant-design-vue";
 
 const loginForm = reactive({
   mobile: "13000000000",
@@ -56,8 +57,27 @@ const sendCode = () => {
   })
   .then((response) => {
     console.log(response);
+    let data = response.data;
+    if (data.success) {
+      notification.success({ description: "验证码发送成功！" });
+      loginForm.code = "8888";
+    } else {
+      notification.error({ description: data.message });
+    }
+  });
+};
+
+const login = () => {
+  axios.post("http://localhost:8000/member/member/login", loginForm).then((response) => {
+    let data = response.data;
+    if (data.success) {
+      notification.success({ description: "登录成功！" });
+      console.log("登录成功：", data.content);
+    } else {
+      notification.error({ description: data.message });
+    }
   })
-}
+};
 </script>
 
 <style scoped>
