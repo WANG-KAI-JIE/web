9eb5f9c7922e4382ea5ae686031874cf8286f349
wkj <1729298243@qq.com>
2023-11-20 18:13:18 +0800

    集成 Ant Design Vue 及其图标

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,9 @@
+
+import { PlusCircleFilled } from '@ant-design/icons-vue';
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <a-button type="primary">测试</a-button>
+    <plus-circle-outlined />
   </div>
 </template>
