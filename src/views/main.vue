0f13ec0c148d4230064a591bb842b51fab73e773
wkj <1729298243@qq.com>
2023-11-20 20:46:42 +0800

    制作 Vue 公共组件

--- src/views/main.vue
+++ src/views/main.vue
@@ -1,94 +1,27 @@
 <template>
   <a-layout id="components-layout-demo-top-side-2">
-    <a-layout-header class="header">
-      <div class="logo" />
-      <a-menu
-        v-model:selectedKeys="selectedKeys1"
-        theme="dark"
-        mode="horizontal"
-        :style="{ lineHeight : '64px'}">
-        <a-menu-item key="1">nav 1</a-menu-item>
-        <a-menu-item key="2">nav 2</a-menu-item>
-        <a-menu-item key="3">nav 3</a-menu-item>
-      </a-menu>
-    </a-layout-header>
+    <the-header-view></the-header-view>
     <a-layout>
-      <a-layout-sider width="200" style="background: #fff">
-        <a-menu
-          v-model:selectedKeys="selectedKeys2"
-          v-model:openKeys="openKeys"
-          mode="inline"
-          :style="{ height : '100%', borderRight: 0 }">
-          <a-sub-menu key="sub1">
-            <template #title>
-              <span>
-                <user-outlined />
-                subnav 1
-              </span>
-            </template>
-            <a-menu-item key="1">option 1</a-menu-item>
-            <a-menu-item key="2">option 2</a-menu-item>
-            <a-menu-item key="3">option 3</a-menu-item>
-            <a-menu-item key="4">option 4</a-menu-item>
-          </a-sub-menu>
-          <a-sub-menu key="sub2">
-            <template #title>
-              <span>
-                <laptop-outlined />
-                subnav 2
-              </span>
-            </template>
-            <a-menu-item key="5">option 5</a-menu-item>
-            <a-menu-item key="6">option 6</a-menu-item>
-            <a-menu-item key="7">option 7</a-menu-item>
-            <a-menu-item key="8">option 8</a-menu-item>
-          </a-sub-menu>
-          <a-sub-menu key="sub3">
-            <template #title>
-              <span>
-                <notification-outlined />
-                subnav 3
-              </span>
-            </template>
-            <a-menu-item key="9">option 9</a-menu-item>
-            <a-menu-item key="10">option 10</a-menu-item>
-            <a-menu-item key="11">option 11</a-menu-item>
-            <a-menu-item key="12">option 12</a-menu-item>
-          </a-sub-menu>
-        </a-menu>
-      </a-layout-sider>
+      <the-sider-view></the-sider-view>
       <a-layout style="padding: 0 24px 24px">
         <a-breadcrumb style="margin: 16px 0">
           <a-breadcrumb-item>Home</a-breadcrumb-item>
           <a-breadcrumb-item>List</a-breadcrumb-item>
           <a-breadcrumb-item>App</a-breadcrumb-item>
         </a-breadcrumb>
-        <a-layout-content 
-          :style="{
-            background: '#fff',
-            padding: '24px',
-            margin: 0,
-            minHeight: '280px'
-          }">
+        <a-layout-content
+            :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
+        >
           Content
         </a-layout-content>
-      </a-layout>;
+      </a-layout>
     </a-layout>
   </a-layout>
 </template>
 
 <script setup>
-import {
-  UserOutlined,
-  LaptopOutlined,
-  NotificationOutlined
-} from '@ant-design/icons-vue';
-
-import { ref } from "vue";
-
-const selectedKeys1 = ref(["2"])
-const selectedKeys2 = ref(["1"])
-const openKeys = ref(["sub1"])
+import TheHeaderView from "@/components/the-header";
+import TheSiderView from "@/components/the-sider.vue";
 </script>
 
 <style scoped>
@@ -99,6 +32,7 @@ const openKeys = ref(["sub1"])
   margin: 16px 24px 16px 0;
   background: rgba(255, 255, 255, 0.3);
 }
+
 .ant-row-rtl #components-layout-demo-top-side-2 .logo {
   float: right;
   margin: 16px 0 16px 24px;
