d63945367587b16848c8c56fd026345d50999b54
wkj <1729298243@qq.com>
2023-11-21 19:17:39 +0800

    前端枚举展示的解决方案-解决表格中枚举字段的显示

--- src/views/main/passenger.vue
+++ src/views/main/passenger.vue
@@ -25,6 +25,14 @@
           <a @click="onEdit(record)">编辑</a>
         </a-space>
       </template>
+      <template v-else-if="column.dataIndex === 'type'">
+        <span v-for="item in PASSENGER_TYPE_ARRAY" :key="item.key">
+          <span v-if="item.key === record.type">
+            {{ item.value }}
+          </span>
+        </span>
+      </template>
+        
     </template>
   </a-table>
 
@@ -48,9 +56,13 @@
       </a-form-item>
       <a-form-item label="类型">
         <a-select v-model:value="passenger.type">
-          <a-select-option value="1">成人</a-select-option>
-          <a-select-option value="2">儿童</a-select-option>
-          <a-select-option value="3">学生</a-select-option>
+          <a-select-option
+            v-for="item in PASSENGER_TYPE_ARRAY"
+            :key="item.key"
+            :value="item.key"
+          >
+            {{ item.value }}
+          </a-select-option>
         </a-select>
       </a-form-item>
     </a-form>
@@ -61,6 +73,12 @@
 import { ref, onMounted } from "vue";
 import { notification } from "ant-design-vue";
 import axios from "axios";
+
+const PASSENGER_TYPE_ARRAY = [
+  { key: "1", value: "成人" },
+  { key: "2", value: "儿童" },
+  { key: "3", value: "学生" },
+];
 
 const visible = ref(false);
 
