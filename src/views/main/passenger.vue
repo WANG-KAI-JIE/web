a87d8c80a964b620a36f17e81f9f9e605fa131e5
wkj <1729298243@qq.com>
2023-11-21 15:59:40 +0800

    乘车人新增界面开发-完成保存功能

--- src/views/main/passenger.vue
+++ src/views/main/passenger.vue
@@ -1,8 +1,8 @@
 <template>
   <a-button type="primary" @click="showModal">新增</a-button>
   <a-modal
-    v-model:visible="visible" 
-    title="乘车人" 
+    v-model:visible="visible"
+    title="乘车人"
     @ok="handleOk"
     ok-text="确认"
     cancel-text="取消"
@@ -12,25 +12,28 @@
       :label-col="{ span: 4 }"
       :wrapper-col="{ span: 20 }"
     >
-    <a-form-item label="姓名">
-      <a-input v-model:value="passenger.name" />
-    </a-form-item>
-    <a-form-item label="身份证">
-      <a-input v-model:value="passenger.idCard" />
-    </a-form-item>
-    <a-form-item label="类型">
-      <a-select v-model:value="passenger.type">
-        <a-select-option value="1">成人</a-select-option>
-        <a-select-option value="2">儿童</a-select-option>
-        <a-select-option value="3">学生</a-select-option>
-      </a-select>
-    </a-form-item>
-  </a-form>
+      <a-form-item label="姓名">
+        <a-input v-model:value="passenger.name" />
+      </a-form-item>
+      <a-form-item label="身份证">
+        <a-input v-model:value="passenger.idCard" />
+      </a-form-item>
+      <a-form-item label="类型">
+        <a-select v-model:value="passenger.type">
+          <a-select-option value="1">成人</a-select-option>
+          <a-select-option value="2">儿童</a-select-option>
+          <a-select-option value="3">学生</a-select-option>
+        </a-select>
+      </a-form-item>
+    </a-form>
   </a-modal>
 </template>
 
 <script setup>
 import { ref, reactive } from "vue";
+import { notification } from "ant-design-vue";
+import axios from "axios";
+
 const visible = ref(false);
 
 const passenger = reactive({
@@ -42,14 +45,22 @@ const passenger = reactive({
   createTime: undefined,
   updateTime: undefined,
 });
+
 const showModal = () => {
   visible.value = true;
-}
+};
 
-const handleOk = (e) => {
-  console.log(e);
-  visible.value = false;
-}
+const handleOk = () => {
+  axios.post("/member/passenger/save", passenger).then((response) => {
+    let data = response.data;
+    if (data.success) {
+      notification.success({ description: "保存成功！" });
+      visible.value = false;
+    } else {
+      notification.error({ description: data.message });
+    }
+  });
+};
 </script>
 
 <style scoped></style>
