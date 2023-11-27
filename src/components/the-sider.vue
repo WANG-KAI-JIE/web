0fe7e77c9de690c337d5dd443c28cdc645e65c1c
wkj <1729298243@qq.com>
2023-11-27 20:39:37 +0800

    会员端增加【我的车票】页面

--- src/components/the-sider.vue
+++ src/components/the-sider.vue
@@ -1,28 +1,35 @@
 <template>
   <a-layout-sider width="200" style="background: #fff">
     <a-menu
-        v-model:selectedKeys="selectedKeys"
-        mode="inline"
-        :style="{ height: '100%', borderRight: 0 }"
+      v-model:selectedKeys="selectedKeys"
+      mode="inline"
+      :style="{ height: '100%', borderRight: 0 }"
     >
       <a-menu-item key="/welcome">
         <router-link to="/welcome">
-          <coffee-outlined/>
+          <coffee-outlined />
           欢迎
         </router-link>
       </a-menu-item>
 
       <a-menu-item key="/passenger">
         <router-link to="/passenger">
-          <user-outlined/>
+          <user-outlined />
           乘车人管理
         </router-link>
       </a-menu-item>
 
       <a-menu-item key="/ticket">
         <router-link to="/ticket">
-          <user-outlined/>
+          <user-outlined />
           余票查询
+        </router-link>
+      </a-menu-item>
+
+      <a-menu-item key="/my-ticket">
+        <router-link to="/my-ticket">
+          <idcard-outlined />
+          我的车票
         </router-link>
       </a-menu-item>
     </a-menu>
@@ -30,19 +37,19 @@
 </template>
 
 <script setup>
-import {ref, watch} from "vue";
+import { ref, watch } from "vue";
 import router from "@/router";
 
 const selectedKeys = ref([]);
 
 watch(
-    () => router.currentRoute.value.path,
-    (newValue) => {
-      console.log("watch", newValue);
-      selectedKeys.value = [];
-      selectedKeys.value.push(newValue);
-    },
-    {immediate: true}
+  () => router.currentRoute.value.path,
+  (newValue) => {
+    console.log("watch", newValue);
+    selectedKeys.value = [];
+    selectedKeys.value.push(newValue);
+  },
+  { immediate: true }
 );
 </script>
 
